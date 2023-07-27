3686c6f537256261588a3b328fbc160a6a34d743
Iryna Shandar <iryna.shandar@gmail.com>
2023-07-27 23:10:49 +0300

    validation regForm

--- js/script.js
+++ js/script.js
@@ -41,12 +41,12 @@
 // // pattern="^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$"
 // // pattern="[0-9a-zA-Z!@#$%^&*]{7,}"
 
-const currentTab = 0; // Current tab is set to be the first tab (0)
+let currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
 function showTab(n) {
   // This function will display the specified tab of the form ...
-  let x = document.getElementsByClassName("tab");
+  const x = document.getElementsByClassName("tab");
   // const arrowRight = document.getElementsByClassName("arrow-right");
   x[n].style.display = "block";
   // ... and fix the Previous/Next buttons:
@@ -68,7 +68,7 @@ function showTab(n) {
 
 function nextPrev(n) {
   // This function will figure out which tab to display
-  let x = document.getElementsByClassName("tab");
+  const x = document.getElementsByClassName("tab");
   // Exit the function if any field in the current tab is invalid:
   if (n == 1 && !validateForm()) return false;
   // Hide the current tab:
@@ -86,33 +86,38 @@ function nextPrev(n) {
 }
 
 function validateForm() {
-  // This function deals with validation of the form fields
-  let x, y, i, valid = true;
-  x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByClassName("email");
-  // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == "") {
-      // add an "invalid" class to the field:
-      // y[i].className += " invalid";
-      document.getElementById("errorEmail").innerHTML = "Please enter a valid email address";
-      document.getElementById("errorEmail").style.background = '#e22a32';
-      // and set the current valid status to false:      
+  let tabClass, inputTag, i, valid = true;
+  tabClass = document.getElementsByClassName("tab");
+  inputTag = tabClass[currentTab].getElementsByTagName("input");
+
+  const validateEmail = (inputEmail)=> inputEmail.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  const validatePassword = (inputPassword)=> inputPassword.value.match(/^[0-9a-zA-Z!@#$%^&*]{7,}$/);
+  // const validateLocation = (inputLocation)=> inputLocation.value.match(/^[0-9a-zA-Z!@#$%^&*, ]{2,} [0-9]{5,}$/);
+
+  for (i = 0; i < inputTag.length; i++) {
+    if (inputTag[i].id == "emailForm" && !validateEmail(emailForm)) {
+      document.getElementById("errorEmailForm").innerHTML = "Please enter a valid email address";
+      document.getElementById("errorEmailForm").style.background = '#e22a32';
       valid = false;
     }
+
+    if (inputTag[i].id == "passwordForm" && !validatePassword(passwordForm)) {
+      document.getElementById("errorPasswordForm").innerHTML = "Please enter a password to secure your account";
+      document.getElementById("errorPasswordForm").style.background = '#e22a32';
+      valid = false;
+    }
+
+    // if (inputTag[i].id == "text" && !validateLocation(text)) {
+    //   document.getElementById("errorLocation").innerHTML = "Enter your postal code to find local matches";
+    //   document.getElementById("errorLocation").style.background = '#e22a32';
+    //   valid = false;
+    // }
   }
-  // If the valid status is true, mark the step as finished and valid:
+
   if (valid) {
     document.getElementsByClassName("step")[currentTab].className += " finish";
   }
-  return valid; // return the valid status
-}
-
-function checkPatern () {
-  const elm  = document.getElementById("email")
-  
-
+  return valid; 
 }
 
 function fixStepIndicator(n) {
@@ -160,3 +165,4 @@ function fixStepIndicator(n) {
 //   const re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
 //   return re.test(String(tel));
 // }
+
